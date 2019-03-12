061bc20eeb8dbeb4f48a434ce6cf456fe8d5eeb6
Junaid Mohamed <junaid98m@gmail.com>
2019-03-12 04:38:37 +0000

    commit

---
+++ app.js
@@ -0,0 +1,47 @@
+//REQUIRED
+//--------------------------------
+var express=require("express"),
+app= express(),
+bodyParser=require("body-parser"),
+methodOverride=require("method-override");
+
+
+
+//OTHER
+//--------------------------
+app.use(express.static("public"));
+
+
+//HOME PAGE ROUTE
+//---------------------------------
+app.get("/", function(req, res){ //takes u to the index (home page)
+    
+    res.render("home.ejs");
+});
+
+//RESUME
+//---------------------------------
+app.get("/resume", function(req, res){ //takes u to the index (home page)
+    
+    res.render("resume.ejs");
+});
+
+//PORTFOLIO
+//-----------------------------
+app.get("/portfolio", function(req, res){ //takes u to the index (home page)
+    
+    res.render("portfolio.ejs");
+});
+
+//CONTACT
+//-----------------------------
+app.get("/contact", function(req, res){ //takes u to the index (home page)
+    
+    res.render("contact.ejs");
+});
+
+
+
+app.listen(process.env.PORT, process.env.IP,  
+function(){console.log("server has started\n");} 
+);
